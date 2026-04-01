1cd7d118b1f47ac1913f4fe6084307ae11391722
chinaCCTVfilm <limingvv789@gmail.com>
2026-04-01 21:43:25 +0800

    V5自动化上线

--- generate.js
+++ generate.js
@@ -3,25 +3,45 @@ const fetch = require("node-fetch");
 
 const API = "https://tmdb.chgov.cc.cd";
 
-async function run() {
-  const res = await fetch(`${API}/movie/popular`);
-  const data = await res.json();
+async function fetchMovies(page) {
+  const res = await fetch(`${API}/movie/popular?page=${page}`);
+  return (await res.json()).results;
+}
 
-  const template = fs.readFileSync("movie-template.html","utf-8");
+async function run() {
+  const template = fs.readFileSync("movie-template.html", "utf-8");
 
   if (!fs.existsSync("movie")) fs.mkdirSync("movie");
 
-  for (let m of data.results) {
-    let html = template
-      .replaceAll("{{title}}", m.title)
-      .replaceAll("{{overview}}", m.overview || "")
-      .replaceAll("{{poster}}", m.poster_path)
-      .replaceAll("{{id}}", m.id);
+  let urls = [];
 
-    fs.writeFileSync(`movie/${m.id}.html`, html);
+  for (let p = 1; p <= 5; p++) {
+    const movies = await fetchMovies(p);
+
+    for (let m of movies) {
+      const file = `movie/${m.id}.html`;
+
+      let html = template
+        .replaceAll("{{title}}", m.title)
+        .replaceAll("{{overview}}", m.overview || "")
+        .replaceAll("{{poster}}", m.poster_path)
+        .replaceAll("{{id}}", m.id);
+
+      fs.writeFileSync(file, html);
+
+      urls.push(`<url><loc>https://decotv666.pages.dev/${file}</loc></url>`);
+    }
   }
 
-  console.log("生成完成");
+  // 生成 sitemap
+  const sitemap = `
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.join("\n")}
+</urlset>`;
+
+  fs.writeFileSync("sitemap.xml", sitemap);
+
+  console.log("V5 自动生成完成");
 }
 
 run();
